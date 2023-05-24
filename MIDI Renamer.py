11dca76b9d8ad32eaf57160e7faad4086ac9845f
4lphabeta <crichards.uk@gmail.com>
2023-05-24 11:50:47 +0100

    Streamlined the code by using a function instead of repeating terms.
    Added an inline variable to the if statement/search function to streamline further and only require the split phrase to be set in one location.
    
    Extra terms added.

--- MIDI Renamer.py
+++ MIDI Renamer.py
@@ -17,91 +17,71 @@ for index, file in enumerate(files):
     curr_split_after = ''
     split_phrase = ''
 
-    if re.search(' - Edit', curr_file):
-        split_phrase = ' - Edit'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search(' -Edit', curr_file):
-        split_phrase = ' -Edit'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search('- Edit', curr_file):
-        split_phrase = '- Edit'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search('Edit ', curr_file):
-        split_phrase = 'Edit '
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search('-  Edit', curr_file):
-        split_phrase = '-  Edit'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search(' - Sesh', curr_file):
-        split_phrase = ' - Sesh'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search('- Sesh', curr_file):
-        split_phrase = '- Sesh'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search('- Wildrage', curr_file):
-        split_phrase = '- Wildrage'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search('- Plasmus642', curr_file):
-        split_phrase = '- Plasmus642'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search('-  Plasmus642', curr_file):
-        split_phrase = '-  Plasmus642'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search(' - WIP', curr_file):
-        split_phrase = ' - WIP'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search('-  VERY WIP', curr_file):
-        split_phrase = '-  VERY WIP'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search(' VERY WIP', curr_file):
-        split_phrase = ' VERY WIP'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search(' WIP', curr_file):
-        split_phrase = ' WIP'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search(' - NF', curr_file):
-        split_phrase = ' - NF'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search(' -NF', curr_file):
-        split_phrase = ' -NF'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search(' - Eman7081', curr_file):
-        split_phrase = ' - Eman7081'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
-    elif re.search(' - Grey42', curr_file):
-        split_phrase = ' - Grey42'
-        curr_split_before = curr_file.split(split_phrase)[0] + '.mid'
-        curr_split_after = curr_file.split(split_phrase)[1]
+    def func_splits(phrase):
+        global curr_split_before, curr_split_after
+        curr_split_before = curr_file.split(phrase)[0] + '.mid'
+        curr_split_after = curr_file.split(phrase)[1]
+
+
+    if re.search(split_phrase := ' - Edit', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' -Edit', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := '- Edit', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := 'Edit ', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' -  Edit', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := '-  Edit', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' - Sesh', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := '- Sesh', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := '- Wildrage', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := '- Plasmus642', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := '-  Plasmus642', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' - WIP', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := '-  VERY WIP', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' VERY WIP', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' WIP', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' - NF', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' -NF', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := '- NF', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' - Eman7081', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' - Grey42', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' - Nohpets', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := '- 10CD', curr_file):
+        func_splits(split_phrase)
+    elif re.search(split_phrase := ' - Jaden Balogh', curr_file):
+        func_splits(split_phrase)
 
     if curr_split_before == next_file:
         os.remove(path + curr_file)
         delete_count += 1
     else:
         if curr_split_after == '.mid':
-            if not os.path.isfile(path + curr_split_before):    # If a correctly named file exists, don't make a new one
+            if not os.path.isfile(path + curr_split_before):  # If a correctly named file exists, don't make a new one
                 new_curr_file = curr_split_before
                 rename = True
         elif curr_split_after == '':
             rename = False
         elif curr_split_after != '':
-            if not os.path.isfile(path + curr_split_before):    # If a correctly named file exists, don't make a new one
+            if not os.path.isfile(path + curr_split_before):  # If a correctly named file exists, don't make a new one
                 new_curr_file = curr_split_before
                 rename = True
 
